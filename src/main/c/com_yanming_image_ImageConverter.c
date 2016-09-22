ea4470ec96ced84d236a651024d3fc10691cf290
allan <yanming3@hotmail.com>
2016-09-22 16:04:40 +0800

    c wand version

---
+++ src/main/c/com_yanming_image_ImageConverter.c
@@ -0,0 +1,391 @@
+#include "com_yanming_image_ImageConverter.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <wand/magick_wand.h>
+#include "magick/enum_strings.h"
+
+JNIEXPORT void JNICALL Java_com_yanming_image_ImageConverter_init
+  (JNIEnv *env, jobject obj)
+  {
+        InitializeMagick(NULL);
+  }
+
+JNIEXPORT void JNICALL Java_com_yanming_image_ImageConverter_destroy
+  (JNIEnv *env, jobject obj)
+  {
+         DestroyMagick();
+  }
+
+
+JNIEXPORT jlong JNICALL Java_com_yanming_image_ImageConverter_newImageHandle
+  (JNIEnv *env, jobject obj)
+  {
+        MagickWand *magick_wand;
+        magick_wand=NewMagickWand();
+        return (jlong)magick_wand;
+  }
+
+
+JNIEXPORT void JNICALL Java_com_yanming_image_ImageConverter_destroyImageHandle
+  (JNIEnv *env, jobject obj, jlong hd)
+  {
+        DestroyMagickWand((MagickWand*)hd);
+  }
+
+
+JNIEXPORT jlong JNICALL Java_com_yanming_image_ImageConverter_getHeight
+  (JNIEnv *env, jobject obj, jlong hd)
+  {
+        unsigned long h;
+        h=MagickGetImageHeight((MagickWand*)hd);
+        return (jlong)h;
+  }
+
+
+JNIEXPORT jlong JNICALL Java_com_yanming_image_ImageConverter_getWidth
+  (JNIEnv *env, jobject obj, jlong hd)
+  {
+        unsigned long w;
+        w=MagickGetImageWidth((MagickWand*)hd);
+        return (jlong)w;
+  }
+
+
+JNIEXPORT jstring JNICALL Java_com_yanming_image_ImageConverter_getFormat
+  (JNIEnv *env, jobject obj, jlong hd)
+  {
+        const char* tStr=MagickGetImageFormat((MagickWand*)hd);
+        jstring result = (*env)->NewStringUTF(env, tStr);
+        return result;
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setFormat
+  (JNIEnv *env, jobject obj, jlong hd, jstring tStr)
+  {
+        MagickPassFail status = MagickPass;
+        const char* t=(*env)->GetStringUTFChars(env,tStr,0);
+        status=MagickSetImageFormat((MagickWand*)hd,t);
+        (*env)->ReleaseStringUTFChars(env, tStr, t);
+        if (status != MagickPass)
+         {
+              char *description;
+              ExceptionType severity;
+
+              description=MagickGetException((MagickWand*)hd,&severity);
+              (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+         }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setCompressQuality
+  (JNIEnv *env, jobject obj, jlong hd, jlong quality)
+  {
+        MagickPassFail status = MagickPass;
+        status=MagickSetCompressionQuality((MagickWand*)hd,(const unsigned long)quality);
+        if (status != MagickPass)
+           {
+              char *description;
+              ExceptionType severity;
+
+              description=MagickGetException((MagickWand*)hd,&severity);
+              (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+           }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setBackgroundColor
+  (JNIEnv *env, jobject obj, jlong hd, jstring color)
+  {
+        const char* c=(*env)->GetStringUTFChars(env,color,0);
+        PixelWand *background;
+        background=NewPixelWand();
+        PixelSetColor(background,c);
+        MagickPassFail status = MagickPass;
+        status=MagickGetImageBackgroundColor((MagickWand*)hd,background);
+        DestroyPixelWand(background);
+        (*env)->ReleaseStringUTFChars(env,color,c);
+        if (status != MagickPass)
+           {
+              char *description;
+              ExceptionType severity;
+
+              description=MagickGetException((MagickWand*)hd,&severity);
+              (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+           }
+           return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setBorderColor
+  (JNIEnv *env, jobject obj, jlong hd, jstring color)
+  {
+          const char* c=(*env)->GetStringUTFChars(env,color,0);
+          PixelWand *background;
+          background=NewPixelWand();
+          PixelSetColor(background,c);
+          MagickPassFail status = MagickPass;
+          status=MagickSetImageBorderColor((MagickWand*)hd,background);
+          DestroyPixelWand(background);
+          (*env)->ReleaseStringUTFChars(env,color,c);
+          if (status != MagickPass)
+           {
+              char *description;
+              ExceptionType severity;
+
+              description=MagickGetException((MagickWand*)hd,&severity);
+              (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+           }
+           return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setDepth
+  (JNIEnv *env, jobject obj, jlong hd, jlong depth)
+  {
+        MagickPassFail status = MagickPass;
+        status=MagickSetImageDepth((MagickWand*)hd, (size_t)depth);
+        if (status != MagickPass)
+         {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+         }
+         return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_setGravity
+  (JNIEnv *env, jobject obj, jlong hd, jstring gravity)
+  {
+        const char* g=(*env)->GetStringUTFChars(env,gravity,0);
+        MagickPassFail status = MagickPass;
+        status=MagickSetImageGravity((MagickWand*)hd,StringToGravityType(g));
+        (*env)->ReleaseStringUTFChars(env,gravity,g);
+        if (status != MagickPass)
+         {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+         }
+         return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_readImage
+  (JNIEnv *env, jobject obj, jlong hd,jstring inFile)
+  {
+        const char* f=(*env)->GetStringUTFChars(env,inFile,0);
+        MagickPassFail status = MagickPass;
+
+        status = MagickReadImage((MagickWand*)hd,f);
+        (*env)->ReleaseStringUTFChars(env, inFile, f);
+        if (status != MagickPass)
+         {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+         }
+         return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_readImageBlob
+  (JNIEnv *env, jobject obj,jlong hd, jbyteArray dArray, jlong len)
+  {
+        MagickPassFail status = MagickPass;
+        const  char* data=(const  char*)(*env)->GetByteArrayElements(env,dArray,0);
+        status=MagickReadImageBlob((MagickWand*)hd,(const unsigned char*)data,(size_t)len),
+        (*env)->ReleaseByteArrayElements(env,dArray,(jbyte*)data,0);
+        if (status != MagickPass)
+        {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+        }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_writeImage
+  (JNIEnv *env, jobject obj, jlong hd, jstring outFile)
+  {
+        const char* f=(*env)->GetStringUTFChars(env,outFile,0);
+        MagickPassFail status = MagickPass;
+        status = MagickWriteImage((MagickWand*)hd,f);
+        (*env)->ReleaseStringUTFChars(env,outFile,f);
+        if (status != MagickPass)
+        {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+        }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_strip
+  (JNIEnv *env, jobject obj, jlong hd)
+  {
+    MagickPassFail status =MagickStripImage((MagickWand*)hd);
+    if (status != MagickPass)
+    {
+        char *description;
+        ExceptionType severity;
+
+        description=MagickGetException((MagickWand*)hd,&severity);
+        (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+    }
+    return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_crop
+  (JNIEnv *env, jobject obj, jlong hd, jlong width, jlong height, jlong x, jlong y)
+  {
+        MagickPassFail status =MagickCropImage((MagickWand*)hd, (const unsigned long)width,(const unsigned long)height, (const long)x, (const long) y);
+        if (status != MagickPass)
+        {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+        }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_scale
+  (JNIEnv *env, jobject obj, jlong hd, jlong columns, jlong rows)
+  {
+        MagickPassFail status =MagickScaleImage((MagickWand*)hd,(const unsigned long)columns,(const unsigned long)rows);
+        if (status != MagickPass)
+        {
+            char *description;
+            ExceptionType severity;
+
+            description=MagickGetException((MagickWand*)hd,&severity);
+            (void) fprintf(stderr,"%.1024s (severity %d)\n",description,severity);
+        }
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_sample
+  (JNIEnv *env, jobject obj, jlong hd, jlong columns,jlong rows)
+  {
+        MagickPassFail status =MagickSampleImage((MagickWand*)hd,(const unsigned long)columns,(const unsigned long)rows);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_sharpen
+  (JNIEnv *env, jobject obj, jlong hd, jdouble radius, jdouble sigma){
+        MagickPassFail status=MagickPass;
+        status=MagickSharpenImage((MagickWand*)hd,(const double)radius,(const double)sigma);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_rotate
+  (JNIEnv *env, jobject obj, jlong hd,jdouble degrees,jstring bStr){
+        MagickPassFail status=MagickPass;
+        const char* b=(*env)->GetStringUTFChars(env,bStr,0);
+        PixelWand *background;
+        background=NewPixelWand();
+        PixelSetColor(background,b);
+        status=MagickRotateImage((MagickWand*)hd,background,(double)degrees);
+        DestroyPixelWand(background);
+        (*env)->ReleaseStringUTFChars(env,bStr,b);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_roll
+  (JNIEnv *env, jobject obj, jlong hd, jlong xOffset,jlong yOffset){
+        MagickPassFail status=MagickPass;
+        status=MagickRollImage((MagickWand*)hd,(const long)xOffset,(const long)yOffset);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_extent
+  (JNIEnv *env, jobject obj, jlong hd, jlong width, jlong height, jlong x, jlong y)
+  {
+        MagickPassFail status=MagickPass;
+        status=MagickExtentImage((MagickWand*)hd,(size_t)width,(size_t)height,(ssize_t)x,(ssize_t)y);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
+
+JNIEXPORT jint JNICALL Java_com_yanming_image_ImageConverter_resize
+  (JNIEnv *env, jobject obj, jlong hd, jlong columns,jlong rows, jdouble blur,jstring f){
+        const char* filterStr=(*env)->GetStringUTFChars(env,f,0);
+        const FilterTypes filter=StringToFilterTypes(filterStr);
+        MagickPassFail status=MagickPass;
+        status=MagickResizeImage((MagickWand*)hd,(const unsigned long)columns,(const unsigned long)rows,filter,(const double)blur);
+        (*env)->ReleaseStringUTFChars(env,f,filterStr);
+        if (status != MagickPass)
+        {
+          char *description;
+          ExceptionType severity;
+
+          description=MagickGetException((MagickWand*)hd,&severity);
+          (void) fprintf(stderr,"%.1024s (severity %d)\n", description,severity);
+        }
+
+        return (jint)(status == MagickPass ? 0 : 1);
+  }
---
+++ src/main/java/com/yanming/image/ImageConverter.java
@@ -0,0 +1,241 @@
+package com.yanming.image;
+
+/**
+ * Created by allan on 16/9/21.
+ */
+public class ImageConverter {
+
+    private long pointer;
+
+
+    public ImageConverter() {
+        this.pointer = newImageHandle();
+    }
+
+    public ImageConverter(String inFile) {
+        this.pointer = newImageHandle();
+        this.readImage(pointer, inFile);
+    }
+
+    public long getHeight() {
+        return this.getHeight(this.pointer);
+    }
+
+    public long getWidth() {
+        return this.getWidth(this.pointer);
+    }
+
+    public String getFormat() {
+        return this.getFormat(this.pointer);
+    }
+
+    public void setFormat(String format) {
+        this.setFormat(this.pointer, format);
+    }
+
+    public void setGravity() {
+        this.setGravity(this.pointer, "Center");
+    }
+
+    public void strip() {
+        this.strip(this.pointer);
+    }
+
+    public void crop(long width, long height, long x, long y) {
+        this.crop(this.pointer, width, height, x, y);
+    }
+
+    public void rotate(double degrees, String background) {
+        this.rotate(this.pointer, degrees, background);
+    }
+
+    public void scale(long columns, long rows) {
+        this.scale(this.pointer, columns, rows);
+    }
+
+    public void sample(long width, long height) {
+        this.sample(this.pointer, width, height);
+    }
+
+    public void resize(long columns, long rows, long blur) {
+        this.resize(this.pointer, columns, rows, blur, "Lanczos");
+    }
+    public  void extent(long width, long height, long x, long y){
+            this.extent(this.pointer,width,height,x,y);
+    }
+
+    public void writeImage(String file) {
+        this.writeImage(this.pointer, file);
+    }
+
+    public void close() {
+        this.destroy();
+    }
+
+    /**
+     * 初始化
+     */
+    private native void init();
+
+    /**
+     * 对象销毁
+     */
+    private native void destroy();
+
+
+    private native long newImageHandle();
+
+    private native void destroyImageHandle(long pointer);
+
+    private native long getHeight(long pointer);
+
+    private native long getWidth(long pointer);
+
+    /**
+     * 获取文件格式
+     *
+     * @param pointer
+     * @return
+     */
+    private native String getFormat(long pointer);
+
+    /**
+     * 设置文件格式
+     *
+     * @param pointer
+     * @param format
+     */
+    private native int setFormat(long pointer, String format);
+
+    /**
+     * 设置压缩质量
+     *
+     * @param pointer
+     * @param quallity
+     */
+    private native int setCompressQuality(long pointer, long quallity);
+
+    /**
+     * 设置背景色
+     *
+     * @param pointer
+     * @param color
+     */
+    private native int setBackgroundColor(long pointer, String color);
+
+    /**
+     * 设置边框颜色
+     *
+     * @param pointer
+     * @param color
+     */
+    private native int setBorderColor(long pointer, String color);
+
+    /**
+     * 设置图像深度
+     *
+     * @param pointer
+     * @param depth
+     */
+    private native int setDepth(long pointer, long depth);
+
+    /**
+     * 设置位置,例如center,northwest
+     *
+     * @param pointer
+     * @param gravity
+     */
+    private native int setGravity(long pointer, String gravity);
+
+    /**
+     * 读取文件
+     *
+     * @param inFile 文件全路径
+     */
+    private native int readImage(long pointer, String inFile);
+
+    /**
+     * 读取文件(二进制)
+     *
+     * @param data 二进制数组
+     * @param size
+     */
+    private native int readImageBlob(long pointer, byte[] data, long size);
+
+    /**
+     * 输出图片
+     *
+     * @param pointer
+     * @param file    图片全路径
+     */
+    private native int writeImage(long pointer, String file);
+
+    /**
+     * 删除图片的元数据,例如Exif信息;
+     *
+     * @param pointer
+     */
+    private native int strip(long pointer);
+
+    /**
+     * 截取图片的部分
+     *
+     * @param pointer
+     * @param width   图片宽度
+     * @param height  图片高度
+     * @param x       起始水平坐标
+     * @param y       起始纵坐标
+     */
+    private native int crop(long pointer, long width, long height, long x, long y);
+
+    /**
+     * 缩放图片到指定尺寸
+     *
+     * @param pointer
+     * @param columns
+     * @param rows
+     */
+    private native int scale(long pointer, long columns, long rows);
+
+    /**
+     * 缩放图片到指定尺寸,和scale的不同在于它是通过像素采样
+     *
+     * @param width
+     * @param height
+     * @return
+     */
+    private native int sample(long pointer, long width, long height);
+
+    /**
+     * 对图片进行高斯锐化,通过增加相邻像素的对比度来使模糊图像变清晰;
+     *
+     * @param pointer
+     * @param radius  半径
+     * @param sigma   对比度
+     */
+    private native int sharpen(long pointer, double radius, double sigma);
+
+    /**
+     * 旋转
+     *
+     * @param degrees
+     * @return
+     */
+    private native int rotate(long pointer, double degrees, String background);
+
+
+    private native int roll(long pointer, long xOffset, long yOffset);
+
+
+    private native int extent(long pointer, long width, long height, long x, long y);
+
+    /**
+     * 通过滤镜缩放图片
+     *
+     * @param columns
+     * @param rows
+     * @param blur    大于1为模糊,小于1为锐化
+     * @return
+     */
+    private native int resize(long pointer, long columns, long rows, double blur, String filter);
+}
